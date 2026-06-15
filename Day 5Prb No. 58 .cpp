0a91ec435610316d9e3cbf442ff448065e1f43c1
Nityayyrrr <nityay1601@gmail.com>
2026-06-15 21:08:56 +0530

    Add Day 5 Problem No. 58 - Length of Last Word

---
+++ Day 5Prb No. 58 .cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int lengthOfLastWord(string s) {
+    int len = 0;
+    int i = s.length() - 1;
+
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+
+    while (i >= 0 && s[i] != ' ') {
+        len++;
+        i--;
+    }
+
+    return len;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    cout << lengthOfLastWord(s);
+
+    return 0;
+}
