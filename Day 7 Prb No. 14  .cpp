ed152b576d7f59c39540360ffd56f702c3ac43fe
Nityayyrrr <nityay1601@gmail.com>
2026-06-17 21:41:31 +0530

    Add Day 7 Problem 14

---
+++ Day 7 Prb No. 14  .cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+
+    string prefix = strs[0];
+
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) {
+            prefix.pop_back();
+
+            if (prefix.empty()) {
+                return "";
+            }
+        }
+    }
+
+    return prefix;
+}
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+
+    cout << "Longest Common Prefix: "
+         << longestCommonPrefix(strs) << endl;
+
+    return 0;
+}
