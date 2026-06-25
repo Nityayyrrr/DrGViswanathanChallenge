2d5fb7fda2c938e7a431e3a7a81a1bf3867feba6
Nityayyrrr <nityay1601@gmail.com>
2026-06-25 22:08:36 +0530

    Add Day 15 Problem No. 204

---
+++ Day 15 Prb No. 204.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countPrimes(int n) {
+    if (n <= 2) return 0;
+
+    vector<bool> isPrime(n, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (long long i = 2; i * i < n; i++) {
+        if (isPrime[i]) {
+            for (long long j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    int count = 0;
+    for (int i = 2; i < n; i++) {
+        if (isPrime[i]) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Number of primes less than " << n << " = "
+         << countPrimes(n) << endl;
+
+    return 0;
+}
