ea7af443d2b6ea05333d73f7a490c14f34d793c9
Nityayyrrr <nityay1601@gmail.com>
2026-06-29 21:22:28 +0530

    Day 19 Prb No. 136 - Single Number (XOR approach)

---
+++ day 19 prb No. 136 .cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+
+    int ans = 0;
+
+    for (int num : nums) {
+        ans ^= num;
+    }
+
+    return ans;
+}
+
+int main() {
+
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Single Number = " << singleNumber(nums);
+
+    return 0;
+}
